27d803fbb5a32bdc22de71bdc13e87743a8ba34b
babisha04 <babishariya@gmail.com>
2025-09-18 10:25:59 +0530

    Added backend API for products

--- frontend/User/products.js
+++ frontend/User/products.js
@@ -1,5 +1,21 @@
-const products=[
+/*const data=[
     {
-    name:"polar", price:"150", category:"pearl", img:""
+    name:"polar", price:"150", category:"pearl"
    }
-]
+]*/
+fetch("http://localhost:8080/api/products")
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById("product-list");
+    container.innerHTML = "";
+    data.forEach(p => {
+      container.innerHTML += `
+        <div class="card col-md-4 p-3">
+          <h5>${p.name}</h5>
+          <p>Price: ₹${p.price}</p>
+          <p>Quantity: ${p.stock}</p>
+          <button class="btn btn-success">Add to Cart</button>
+        </div>`;
+    });
+  })
+  .catch(err => console.error("Error:", err));
